edb0fd7d29b1ca64c3e5a52ddb8f7bffe0629764
Nikolay Chernov <n_chernov@wargaming.net>
2017-11-24 15:05:14 +0300

    connection dialog

---
+++ TrainObserver/resource.h
@@ -0,0 +1,23 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by TrainObserver.rc
+//
+#define IDD_ABOUTBOX                    100
+#define IDR_MAINFRAME                   128
+#define IDD_CONNECTDLG                  129
+#define IDC_COMBO_SUCCESS               1000
+#define IDC_EDIT_CONNECT_ADDR           1001
+#define IDC_EDIT_CONNECT_PORT           1002
+#define IDC_EDIT                        1003
+#define IDC_EDIT_CONNECT_USER_NAME      1003
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        202
+#define _APS_NEXT_COMMAND_VALUE         32775
+#define _APS_NEXT_CONTROL_VALUE         1004
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif

18e6d529d78f3f8c71cc32b1232065b9d2527e2c
Vasissualy <32646276+Vasissualy@users.noreply.github.com>
2017-12-23 20:51:45 +0300

    ui fix

--- TrainObserver/space_ui.cpp
+++ TrainObserver/space_ui.cpp
@@ -20,9 +20,9 @@ enum class UIObjectType : uint
 const uint COLOR_COUNT = 4;
 
 const DWORD colors[COLOR_COUNT] = {D3DCOLOR_ARGB(255, 210, 145, 20),
-						 D3DCOLOR_ARGB(255, 210, 250, 180),
-						 D3DCOLOR_ARGB(255, 180, 210, 250),
-						 D3DCOLOR_ARGB(255, 245, 180, 250)};
+								   D3DCOLOR_ARGB(255, 210, 250, 180),
+								   D3DCOLOR_ARGB(255, 180, 210, 250),
+								   D3DCOLOR_ARGB(255, 245, 180, 250)};
 
 
 uint generateUIIndex(UIObjectType objType, uint objIdx, uint uiElementIdx)
@@ -55,7 +55,7 @@ void createPostUI(const Vector3& pos, const Post& post)
 			case EPostType::CITY:
 				sprintf_s(
 					buf,
-					"%s\npopulation: %d/%d\nproduct: %d/%d\narmor: %d/%d",
+					"%s\npopulation: %d / %d\nproduct: %d / %d\narmor: %d / %d",
 					post.name.c_str(),
 					post.population,
 					post.population_capacity,
@@ -67,12 +67,12 @@ void createPostUI(const Vector3& pos, const Post& post)
 				controlSize.y = 70;
 				break;
 			case EPostType::MARKET:
-				sprintf_s(buf, "%s\nproduct: %d", post.name.c_str(), post.product);
-				controlSize.x = 80;
+				sprintf_s(buf, "%s\nproduct: %d / %d", post.name.c_str(), post.product, post.product_capacity);
+				controlSize.x = 90;
 				controlSize.y = 40;
 				break;
 			case EPostType::MILITARY_STORAGE:
-				sprintf_s(buf, "%s\narmor: %d", post.name.c_str(), post.armor);
+				sprintf_s(buf, "%s\narmor: %d / %d", post.name.c_str(), post.armor, post.armor_capacity);
 				controlSize.x = 80;
 				controlSize.y = 40;
 				break;
@@ -107,10 +107,10 @@ void createTrainUI(const Vector3& pos, const Train& train)
 	if (isInScreen)
 	{
 		static std::unordered_map<std::string, DWORD> player_color;
-		static int color_num = 0;
+		static int									  color_num = 0;
 
 		DWORD color = 0;
-		auto it = player_color.find(train.player_id);
+		auto  it = player_color.find(train.player_id);
 		if (it == player_color.end())
 		{
 			color = colors[color_num++ % COLOR_COUNT];
@@ -123,7 +123,13 @@ void createTrainUI(const Vector3& pos, const Train& train)
 
 		char	  buf[512];
 		ScreenPos controlSize = {100, 40};
-		sprintf_s(buf, "player: %s\ngoods: %d/%d\nlvl: %d", train.player_id.c_str(), train.goods, train.goods_capacity, train.level);
+		sprintf_s(
+			buf,
+			"player: %s\ngoods: %d/%d\nlvl: %d",
+			train.player_id.c_str(),
+			train.goods,
+			train.goods_capacity,
+			train.level);
 		controlSize.x = 80;
 		controlSize.y = 60;
 
